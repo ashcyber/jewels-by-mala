8079d4fdbcaf120d8fbb030bf0d737540333f848
aishwarya <ishans632@gmail.com>
2019-10-26 16:01:32 +0530

    Updates

---
+++ precache-manifest.d77bcab2bd684e25765cf1c90c6dc4a8.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134bf3d2c22cb48bfed34f5519f3b9dc",
+    "url": "/jewels-by-mala/index.html"
+  },
+  {
+    "revision": "52e8f607af16ffd2cc61",
+    "url": "/jewels-by-mala/static/css/2.06d56bc8.chunk.css"
+  },
+  {
+    "revision": "a8a93f4dd0eced3a6d7d",
+    "url": "/jewels-by-mala/static/css/main.076f76d1.chunk.css"
+  },
+  {
+    "revision": "52e8f607af16ffd2cc61",
+    "url": "/jewels-by-mala/static/js/2.76150846.chunk.js"
+  },
+  {
+    "revision": "a8a93f4dd0eced3a6d7d",
+    "url": "/jewels-by-mala/static/js/main.303f49d4.chunk.js"
+  },
+  {
+    "revision": "18cffbbf3c4d70391ba3",
+    "url": "/jewels-by-mala/static/js/runtime-main.16fe5e30.js"
+  },
+  {
+    "revision": "cec8a6834241575dcafba6d7504d64b8",
+    "url": "/jewels-by-mala/static/media/BOD_I_0.cec8a683.TTF"
+  },
+  {
+    "revision": "26692b256ea55fd9f2ff4bca05fbe1b7",
+    "url": "/jewels-by-mala/static/media/about-background.26692b25.png"
+  },
+  {
+    "revision": "e7b9d158c1c0b278fc0a49a13affd39b",
+    "url": "/jewels-by-mala/static/media/artist-image.e7b9d158.png"
+  },
+  {
+    "revision": "1ba357e6d7fcbed5e1c74a89712ec579",
+    "url": "/jewels-by-mala/static/media/burger.1ba357e6.svg"
+  },
+  {
+    "revision": "e51543371ed52a6db440bce60c19fb5c",
+    "url": "/jewels-by-mala/static/media/d-earings-1.e5154337.png"
+  },
+  {
+    "revision": "25e00b8a0bb5c80e3ff3014320e6976f",
+    "url": "/jewels-by-mala/static/media/d-earings-2.25e00b8a.png"
+  },
+  {
+    "revision": "198c7317b1b3c9558a553982f728fbe8",
+    "url": "/jewels-by-mala/static/media/d-earings-3.198c7317.png"
+  },
+  {
+    "revision": "3b8b4538efbf2f30a95aea2976c6bf7a",
+    "url": "/jewels-by-mala/static/media/d-necklace-1.3b8b4538.png"
+  },
+  {
+    "revision": "ae5d373fbd8737e7240a505e9c5962ae",
+    "url": "/jewels-by-mala/static/media/d-necklace-2.ae5d373f.png"
+  },
+  {
+    "revision": "e7cf700a3bd0b70e7ddd2794cd9e30db",
+    "url": "/jewels-by-mala/static/media/d-necklace-3.e7cf700a.png"
+  },
+  {
+    "revision": "6da56b84004bffe9eda06a359acd5407",
+    "url": "/jewels-by-mala/static/media/diamond-earings-background.6da56b84.jpg"
+  },
+  {
+    "revision": "206b0cb678a50155d250b02b9b710356",
+    "url": "/jewels-by-mala/static/media/diamond-necklaces-background.206b0cb6.png"
+  },
+  {
+    "revision": "84538bdaf41eb6ac7d61214e9af9d711",
+    "url": "/jewels-by-mala/static/media/main-background.84538bda.png"
+  },
+  {
+    "revision": "bee396f5e15c45db339230f2eddf9718",
+    "url": "/jewels-by-mala/static/media/main-logo.bee396f5.png"
+  },
+  {
+    "revision": "674c0ffbac1c26d400464c6fdc629778",
+    "url": "/jewels-by-mala/static/media/main-title.674c0ffb.png"
+  },
+  {
+    "revision": "185d8287d5c7b8145b55a794211e3204",
+    "url": "/jewels-by-mala/static/media/nav-logo.185d8287.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/jewels-by-mala/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/jewels-by-mala/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/jewels-by-mala/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/jewels-by-mala/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a6f467a9579d327848a4fd33d4414bf9",
+    "url": "/jewels-by-mala/static/media/testimonials-background.a6f467a9.png"
+  },
+  {
+    "revision": "2c107fd254f69529b6360eccfd4a8a3d",
+    "url": "/jewels-by-mala/static/media/uncut-bangles-background.2c107fd2.jpg"
+  },
+  {
+    "revision": "d0e64e06f55c095ad91fe675b02a437a",
+    "url": "/jewels-by-mala/static/media/uncut-earings-background.d0e64e06.jpg"
+  }
+]);
